194e46ebeee2f8d5f1d1453ad6a237e00392c97e
Roberto Carletto <roberto.carletto@edu.unito.it>
2022-06-15 22:53:34 +0200

    sql delete

--- delete.sql
+++ delete.sql
@@ -1,1 +1,25 @@
-DELETE FROM table_name WHERE condition;
+--Check delete costrains works
+DELETE FROM Utenti WHERE CodiceUtente = 1;
+--
+DELETE FROM Stati WHERE CodiceStato = 1;
+--
+DELETE FROM Regioni WHERE CodiceRegione = 1;
+--
+DELETE FROM Comuni WHERE CodiceComune = 1;
+--
+DELETE FROM Citta WHERE CodiceCitta = 1;
+--
+DELETE FROM Host WHERE CodiceHost = 1;
+--
+DELETE FROM Alloggi WHERE CodiceAlloggio = 1;
+--
+DELETE FROM Servizi WHERE CodiceServizio = 1;
+--
+DELETE FROM MetodiPagamento WHERE CodiceMetodo = 1;
+--
+DELETE FROM Prenotazione WHERE CodicePrenotazione = 1;
+--
+DELETE FROM Liste WHERE CodiceLista = 1;
+--
+DELETE FROM Messaggi WHERE CodiceMessaggio = 1;
+--
